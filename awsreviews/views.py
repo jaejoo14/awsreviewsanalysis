304b82d67bb91b810954c3f479dd83818fbc9d32
Jae <jaejoo14@gmail.com>
2019-08-02 16:55:10 -0400

    final for blog

--- awsreviews/views.py
+++ awsreviews/views.py
@@ -5,6 +5,6 @@ import json
 # Create your views here.
 def search_list(request):
 
-    searchs=Search.objects.all()
+    search_result=list(Search.objects.all())[0]
 	
-    return render(request, 'awsreviews/search_list.html', {'searchs':searchs})
+    return render(request, 'awsreviews/search_list.html', {'search_result':search_result })
