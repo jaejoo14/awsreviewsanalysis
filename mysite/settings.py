588a9c43db9323b09b5d91aadc2f88e67fb7cbc3
EC2 Default User <ec2-user@ip-172-31-32-177.us-east-2.compute.internal>
2019-08-06 01:21:04 +0000

    ALLOWED HOST

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'llystm*nx5ad2newrj6-9%oixh9bbl%70lnhe_62t1d#oah8ii'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-13-58-200-232.us-east-2.compute.amazonaws.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
